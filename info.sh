2eb0172de8aa50f5beb860ef27589db795f3b56b
Ceand1 <78984017+Ceand1@users.noreply.github.com>
2022-12-11 21:51:17 -0300

    Update info.sh

--- info.sh
+++ info.sh
@@ -10,13 +10,13 @@ do
    CARGA1=$(uptime | cut -f3 -d',')
    CARGA2=$(uptime | cut -f4 -d',')
    CARGA3=$(uptime | cut -f5 -d',')
-   echo "Carga media do ultimo minuto: $CARGA1 <br>" >> /var/www/html/index.html
-   echo "Carga media dos ultimos cinco  minutos: $CARGA2 <br>" >> /var/www/html/index.html
-   echo "Carga media dos ultimos quinze minutos: $CARGA3 <br>" >> /var/www/html/index.html
+   echo "Carga media 1min: $CARGA1 <br>" >> /var/www/html/index.html
+   echo "Carga media 5min: $CARGA2 <br>" >> /var/www/html/index.html
+   echo "Carga media 15min: $CARGA3 <br>" >> /var/www/html/index.html
    MEMORIALIVRE=$(free | tr -s ' ' | grep "Mem" | cut -f3 -d' ')
    MEMORIAOCUPADA=$(free | tr -s ' ' | grep "Mem" | cut -f4 -d' ')
-   echo "Memoria livre do sistema: $MEMORIALIVRE <br>" >> /var/www/html/index.html
-   echo "Memoria ocupada do sistema: $MEMORIAOCUPADA <br>" >> /var/www/html/index.html
+   echo "Memoria livre: $MEMORIALIVRE <br>" >> /var/www/html/index.html
+   echo "Memoria ocupada: $MEMORIAOCUPADA <br>" >> /var/www/html/index.html
    BYTESR=$(cat /proc/net/dev | tr -s ' '| grep "eth0" | cut -f3 -d' ')
    BYTERE=$(cat /proc/net/dev | tr -s ' '| grep "eth0" | cut -f11 -d' ')
    echo "Bytes recebidos pela interface eth0: $BYTESR <br>" >> /var/www/html/index.html
